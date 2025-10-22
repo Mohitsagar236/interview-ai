fb0a9ba65299fd9a6921adf67e07b604794e020b
cp813 <cp8137108@gmail.com>
2025-10-22 06:15:51 +0530

    new web

--- scripts/prepare-build.js
+++ scripts/prepare-build.js
@@ -111,17 +111,9 @@ async function verifyBuild() {
   
   // Update package.json extraResources with correct site-packages path
   if (packageJson.build && packageJson.build.extraResources) {
-    // Update the site-packages path or add if not present
-    let sitePackagesResource = packageJson.build.extraResources.find(res => res.to === 'python/site-packages');
-    if (!sitePackagesResource) {
-      sitePackagesResource = {
-        "from": venvSitePackages + '/',
-        "to": "python/site-packages",
-        "filter": ["**/*"]
-      };
-      packageJson.build.extraResources.push(sitePackagesResource);
-      log('Added site-packages entry to extraResources.');
-    } else {
+    // Update the site-packages path
+    const sitePackagesResource = packageJson.build.extraResources.find(res => res.to === 'python/site-packages');
+    if (sitePackagesResource) {
       sitePackagesResource.from = venvSitePackages + '/';
     }
     fs.writeFileSync(path.join(rootDir, 'package.json'), JSON.stringify(packageJson, null, 2));
